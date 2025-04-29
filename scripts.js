a3447276bb2f6e5b2c634b823e9d90446b295bba
RenanBatista17 <renanjr381@gmail.com>
2025-04-29 01:31:09 -0300

    api-mercadolivre-add

--- scripts.js
+++ scripts.js
@@ -15,10 +15,10 @@ searchForm.addEventListener('submit', async function (event) {
 function displayItems(products) {
     productList.innerHTML = products.map(product => `
         <div class="product-card">
-            <img src="${product.thumbnail}" alt="${product.title}">
+            <img src="${product.thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}" alt="${product.title}">
             <h3>${product.title}</h3>
-            <p>${product.price.toLocaleString('pt-br', { style: "currency", currency: "BRL" })}</p>
-            <p>Loja: ${product.seller.nickname}</p>
+            <p class="product-price">${product.price.toLocaleString('pt-br', { style: "currency", currency: "BRL" })}</p>
+            <p class="product-store">Loja: ${product.seller.nickname}</p>
         </div>
         `,).join('')
 }
